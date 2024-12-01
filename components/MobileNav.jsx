9e2f2f32dd451cb5ce5d19ac3201c0d22399c7e3
Hamilton8560 <60953362+Hamilton8560@users.noreply.github.com>
2024-11-30 20:36:55 -0500

    Update MobileNav.jsx

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger, SheetClose } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
@@ -10,23 +10,10 @@ const links = [
     name: "home",
     path: "/",
   },
-
   {
-    name:"about me",
-    path:"/about-me"
+    name: "about me",
+    path: "/about-me",
   },
-  // {
-  //   name: "services",
-  //   path: "/work",
-  // },
-  // {
-  //   name: "resume",
-  //   path: "/resume",
-  // },
-  // {
-  //   name: "work",
-  //   path: "/work",
-  // },
   {
     name: "contact",
     path: "/contact",
@@ -53,16 +40,17 @@ const MobileNav = () => {
         <nav className="flex flex-col justify-center items-center gap-8">
           {links.map((link, index) => {
             return (
-              <Link
-                href={link.path}
-                key={index}
-                className={`${
-                  link.path === pathname &&
-                  "text-accent border-b-2 border-accent"
-                } text-xl capitalize hover:text-accent transition-all`}
-              >
-                {link.name}
-              </Link>
+              <SheetClose asChild key={index}>
+                <Link
+                  href={link.path}
+                  className={`${
+                    link.path === pathname &&
+                    "text-accent border-b-2 border-accent"
+                  } text-xl capitalize hover:text-accent transition-all`}
+                >
+                  {link.name}
+                </Link>
+              </SheetClose>
             );
           })}
         </nav>
